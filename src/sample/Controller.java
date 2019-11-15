eb965f1fbe61a44ae94186ec7c9ba851021cfb70
SimretA <simretaraya7@gmail.com>
2019-11-15 22:27:28 +0300

    UI works

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,18 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -13,6 +21,10 @@ public class Controller {
     @FXML Pane definitionPane;
     @FXML TextField wordInput;
     @FXML Text wordOutput;
+    @FXML TextField word;
+    @FXML TextArea definition;
+    @FXML Label warning;
+
     public void search(MouseEvent mouseEvent) {
 
         Client client = new Client("127.0.0.1",5000);
@@ -24,6 +36,43 @@ public class Controller {
         }
         //wordOutput.setText(wordInput.getText());
         definitionPane.setVisible(true);
+
+    }
+
+    public void openAddWordDialog(MouseEvent mouseEvent) {
+
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("addWord.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(new Scene(root));
+        stage.setTitle("Add Word");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
+    }
+
+    public void addWord(MouseEvent mouseEvent) {
+        if(word.getText().trim().isEmpty() || definition.getText().trim().isEmpty()){
+            warning.setVisible(true);
+
+
+            return;
+        }
+
+        Client client = new Client("127.0.0.1",5000);
+        SocketObject socketObject = new SocketObject(new Word(word.getText(),definition.getText()),"POST");
+        try {
+            client.sendRequest(socketObject);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ((Node)(mouseEvent.getSource())).getScene().getWindow().hide();
+
+    }
+    public void deleteWord(MouseEvent mouseEvent){
 
     }
 }
